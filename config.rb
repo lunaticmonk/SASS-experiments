bf6cb3a40d11e4c53f70c436b4790c7514984d99
Sumedh Nimkarde <blesssumedh@gmail.com>
2016-10-14 19:29:18 +0530

    prototype ready

--- config.rb
+++ config.rb
@@ -13,6 +13,8 @@ javascripts_dir = "js"
 output_style = :expanded
 # To enable relative paths to assets via compass helper functions. Uncomment:
 relative_assets = true
+Compass::BrowserSupport.add_support("function-name", "webkit", "moz")
+
 
 # To disable debugging comments that display the original location of your selectors. Uncomment:
 # line_comments = false
--- js/main.js
+++ js/main.js
@@ -3,9 +3,16 @@
 	var home_btn = document.querySelector('#home_btn');
 	var about_btn = document.querySelector('#about_btn');
 	var contribute_btn = document.querySelector('#contribute_btn');
+	var idk_btn = document.querySelector('#idk_btn');
+
 	menu_btn.addEventListener('click',function(){
-	home_btn.classList.add('move-home');		
-	about_btn.classList.add('move-about');
-	contribute_btn.classList.add('move-contribute');
+		home_btn.classList.toggle('move-home');		
+		about_btn.classList.toggle('move-about');
+		contribute_btn.classList.toggle('move-contribute');
+		idk_btn.classList.toggle('move-idk');
+
+		// menu_btn.addEventListener('mouseleave',function(){
+		// 	home_btn.classList.add('rev-menu');
+		// });
 	});
 })();
